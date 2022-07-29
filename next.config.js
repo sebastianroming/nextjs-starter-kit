392b3b1b5d31dfda3143a41601583992915f23b6
Sebastian Roming <sebastian@webmonkey.io>
2022-07-29 14:05:15 +0200

    api test

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = {  
   reactStrictMode: true,
+  swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: "/./hello",
+        destination: "/api/hello",
+        permanent: false
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
