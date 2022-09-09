087c064b968ce0eb89ebe567dec52e1e670f082e
Sebastian Roming <sebastian@webmonkey.io>
2022-09-09 17:06:42 +0200

    middleware

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,24 +1,21 @@
-import { NextResponse } from 'next/server'
+import { NextResponse } from "next/server";
+export async function middleware(req) {
 
-// Block Austria, prefer Germany
-const BLOCKED_COUNTRY = 'AT'
+  console.log("request", req);
 
-// Limit middleware pathname config
-export const config = {
-  matcher: '/',
-}
-
-export function middleware(req) {
-  // Extract country
-  const country = req.geo.country || 'US'
-
-  // Specify the correct pathname
-  if (country === BLOCKED_COUNTRY) {
-    req.nextUrl.pathname = '/blocked'
-  } else {
-    req.nextUrl.pathname = `/${country}`
-  }
-
-  // Rewrite to URL
-  return NextResponse.rewrite(req.nextUrl)
+  const nextUrl = req.nextUrl;
+  const { geo } = req;
+  const country = geo.country || "NULL";
+  const city = geo.city || "NULL";
+  const region = geo.region || "NULL";
+  const timezone = geo.timezone || "NULL";
+  const ip = geo.ip || "NULL";
+  nextUrl.searchParams.set("country", country);
+  nextUrl.searchParams.set("city", city);
+  nextUrl.searchParams.set("region", region);
+  nextUrl.searchParams.set("timezone", timezone);
+  nextUrl.searchParams.set("ip", ip);
+  console.log("NextURL", nextUrl);
+  NextResponse.rewrite(nextUrl);
+  return NextResponse.rewrite(nextUrl);
 }
