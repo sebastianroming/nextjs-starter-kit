7597e051bb69a2b4e92f92a7c6a1ba28b8a5ae15
Sebastian Roming <200112+sebastianroming@users.noreply.github.com>
2023-01-17 07:26:14 +0100

    Test PR

--- pages/index.js
+++ pages/index.js
@@ -24,6 +24,7 @@ export default function Home() {
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h2>Documentation &rarr;</h2>
+<h2>TEst</h2>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
