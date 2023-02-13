c747eda1da0e1975fd7c1c50fe2d28d922aefede
Sebastian Roming <200112+sebastianroming@users.noreply.github.com>
2023-02-13 13:41:50 +0100

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,9 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default async function handler(req, res) {
+export default function handler(req, res) {
   console.log(process.version);
   //const country = req.headers.get('x-vercel-ip-country');
-  const country = await req.headers.get('x-vercel-ip-country');
+  const country = req.headers['x-vercel-ip-country'];
   console.log(country);
   res.status(200).json({ name: 'John Doe', country })
 }
