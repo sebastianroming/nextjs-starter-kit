010bde2e10cd28c2e05bc5eb815c2feb532647ef
Sebastian Roming <sebastian@webmonkey.io>
2023-09-07 13:44:22 +0200

    middleware

--- middleware.js
+++ middleware.js
@@ -1,24 +1,6 @@
-// This function can be marked `async` if using `await` inside
-/*export function middleware(request) {
-  console.log(`Middleware request.referrer: '${request.referrer}'`)
-  console.log(`Middleware request.headers.get('referer'): '${request.headers.get('referer')}'`)
-
-  console.log("nextUrl", request.nextUrl);
-  const host = request.nextUrl.hostname || undefined;
-  console.log("host", host);
-
-  console.log("process.env.lowercase_UPPERCASE", process.env.lowercase_UPPERCASE);
-  
-}
-*/
 
 import { NextResponse } from 'next/server';
  
-// config with custom matcher
-//export const config = {
-//  matcher: '/redirectTest',
-//};
-
 export const config = {
   matcher: [
     /*
@@ -32,8 +14,7 @@ export const config = {
      * - sitemap.html (sitemap file for humans)
      * - /br (Brazilian version of the site)
      */
-    "/((?!api|_next/static|_next/image|_next/data|favicon.ico|sitemap.xml|robots.txt|sitemap.html|/br|(?!/br\\?paginas=.*)).*)",
-    ""
+    "/((?!api|_next/static|_next/image|_next/data|favicon.ico|sitemap.xml|robots.txt|sitemap.html|br|(?!br/:path\\?paginas=.*)).*)"
   ],
 };
  
@@ -43,27 +24,9 @@ export default function middleware(request) {
 
   const referrer = request.referrer || 'not specified';
 
-  //const requestHeaders = new Headers(request.headers)
- // requestHeaders.set('X-Custom-Referrer-Header', referrer)
-
-  //request.headers.set('X-Custom-Referrer-Header', referrer)
-  //request.headers.set('Referer', referrer)
-
-  //console.log('referrer', referrer);
-
-
-  //const response = NextResponse.redirect(new URL('/api/referer', request.url));
-  //response.headers.set('X-Custom-Referrer-Header2', referrer);
-  //response.headers.set('Referer', referrer)
-  //console.log('referrer 2', referrer);
-
-
-
   const _redirect = new Response();
   _redirect.headers.set('X-Custom-Referrer-Header', referrer);
 
   return _redirect.redirect(new URL('/api/referer', request.url));;
 
-
-  //return request.redirect(new URL('/api/referer', request.url));
 }
